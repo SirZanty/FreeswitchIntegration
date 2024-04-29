56406532b19c43ab0ed19a3bc2850c6a7c6bf11f
SirZanty <sirzanty@gmail.com>
2024-04-28 22:57:33 -0500

    out

--- Program.cs
+++ Program.cs
@@ -8,23 +8,43 @@ using NEventSocket.FreeSwitch;
 
 NEventSocket.Logging.Logger.Configure(new LoggerFactory());
 
-using (var socket = await InboundSocket.Connect("localhost", 8021, "ClueCon"))
+using (var listener = new OutboundListener(8084))
 {
-    var apiResponse = await socket.SendApi("status");
-    Console.WriteLine(apiResponse.BodyText);
+    listener.Connections.Subscribe(
+      async socket => {
+          await socket.Connect();
 
-    //Tell FreeSwitch which events we are interested in
-    await socket.SubscribeEvents(EventName.ChannelAnswer);
+          //after calling .Connect(), socket.ChannelData
+          //is populated with all the headers and variables of the channel
 
-    //Handle events as they come in using Rx
-    socket.ChannelEvents.Where(x => x.EventName == EventName.ChannelAnswer)
-          .Subscribe(async x =>
-          {
-              Console.WriteLine("Channel Answer Event " + x.UUID);
+          var uuid = socket.ChannelData.Headers[HeaderNames.UniqueId];
+          Console.WriteLine("OutboundSocket connected for channel " + uuid);
 
-              //we have a channel id, now we can control it
-              await socket.Play(x.UUID, "/tmp/audioFinal1.wav");
-          });
+          await socket.SubscribeEvents(EventName.ChannelHangup);
+
+          socket.ChannelEvents
+              .Where(x => x.EventName == EventName.ChannelHangup && x.UUID == uuid)
+              .Take(1)
+              .Subscribe(async x => {
+                  Console.WriteLine("Hangup Detected on " + x.UUID);
+                  await socket.Exit();
+              });
+
+
+          //if we use 'full' in our FS dialplan, we'll get events for ALL channels in FreeSwitch
+          //this is not desirable here - so we'll filter in for our unique id only
+          //cases where this is desirable is in the channel api where we want to catch other channels bridging to us
+          await socket.Filter(HeaderNames.UniqueId, uuid);
+
+          //tell FreeSwitch not to end the socket on hangup, we'll catch the hangup event and .Exit() ourselves
+          await socket.Linger();
+
+          await socket.ExecuteApplication(uuid, "answer");
+          await socket.Play(uuid, "misc/8000/misc-freeswitch_is_state_of_the_art.wav");
+          await socket.Hangup(uuid, HangupCause.NormalClearing);
+      });
+
+    listener.Start();
 
     Console.WriteLine("Press [Enter] to exit.");
     await Util.WaitForEnterKeyPress();
