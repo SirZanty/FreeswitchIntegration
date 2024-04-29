3310887dc1c234bfefa9be260691f87e7e9fe103
SirZanty <sirzanty@gmail.com>
2024-04-28 22:30:30 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FreeswitchIntegration;
+using Microsoft.Extensions.Logging;
+using NEventSocket;
+using NEventSocket.FreeSwitch;
+
+NEventSocket.Logging.Logger.Configure(new LoggerFactory());
+
+using (var socket = await InboundSocket.Connect("localhost", 8021, "ClueCon"))
+{
+    var apiResponse = await socket.SendApi("status");
+    Console.WriteLine(apiResponse.BodyText);
+
+    //Tell FreeSwitch which events we are interested in
+    await socket.SubscribeEvents(EventName.ChannelAnswer);
+
+    //Handle events as they come in using Rx
+    socket.ChannelEvents.Where(x => x.EventName == EventName.ChannelAnswer)
+          .Subscribe(async x =>
+          {
+              Console.WriteLine("Channel Answer Event " + x.UUID);
+
+              //we have a channel id, now we can control it
+              await socket.Play(x.UUID, "/tmp/audioFinal1.wav");
+          });
+
+    Console.WriteLine("Press [Enter] to exit.");
+    await Util.WaitForEnterKeyPress();
+}
---
+++ Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeswitchIntegration;
+
+class Util
+{
+    public static Task WaitForEnterKeyPress()
+    {
+        return
+            Observable.Interval(TimeSpan.FromMilliseconds(100))
+                .Where(_ => Console.KeyAvailable)
+                .Select(_ => Console.ReadKey(false).Key)
+                .FirstAsync(x => x == ConsoleKey.Enter)
+                .ToTask();
+    }
+}
